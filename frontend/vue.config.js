e569ed1e86f4aa5d8880cfcefcd21dccf622e554
DanielBlack2718 <danyablack278@mail.ru>
2023-10-09 05:52:05 +0500

    Делаю dockerЭ

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,15 +4,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Замените на URL вашего бэкенда
+      '/public': {
+        target: 'http://localhost:8000', // Замените на URL вашего бэкенда
         changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-      },
-      '/storage': {
-        target: 'http://localhost:8080/storage',
-        changeOrigin: true,
-      },
+      }
     },
   },
 })
